6cd7f870583f19f01b7f5e34b8addf5ed949ae68
Alejandro <ja.jimenezrosa@gmail.com>
2020-03-03 15:45:35 -0400

    my fist commit prueba

---
+++ index.js
@@ -0,0 +1,33 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const server_1 = __importDefault(require("./classes/server"));
+const usuarios_1 = __importDefault(require("./routes/usuarios"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const express_fileupload_1 = __importDefault(require("express-fileupload"));
+const post_route_1 = __importDefault(require("./routes/post.route"));
+const cors_1 = __importDefault(require("cors"));
+const server = new server_1.default();
+//Body parser
+server.app.use(body_parser_1.default.urlencoded({ extended: true }));
+server.app.use(body_parser_1.default.json());
+//FileUpload
+server.app.use(express_fileupload_1.default({ useTempFiles: true }));
+//Configurar CORS
+server.app.use(cors_1.default({ origin: true, credentials: true }));
+//Rutas de mi aplicacion
+server.app.use('/user', usuarios_1.default);
+server.app.use('/post', post_route_1.default);
+//Conectar DB
+mongoose_1.default.connect('mongodb://localhost:27017/fotosgram', { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }, (err) => {
+    if (err)
+        throw err;
+    console.log('Base de datos ONLINE!');
+});
+//Levantar Express
+server.start(() => {
+    console.log(`Servidor Corriendo en Puerto ${server.port} `);
+});
