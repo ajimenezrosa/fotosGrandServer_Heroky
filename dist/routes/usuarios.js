d355909be9b64b5035cc99bcaf812ede42fac392
Alejandro <ja.jimenezrosa@gmail.com>
2020-03-03 15:40:06 -0400

    my fist commit

---
+++ dist/routes/usuarios.js
@@ -0,0 +1,111 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const usuario_model_1 = require("../models/usuario.model");
+const bcrypt_1 = __importDefault(require("bcrypt"));
+const token_1 = __importDefault(require("../classes/token"));
+const autenticacion_1 = require("../middlewares/autenticacion");
+const userRourtes = express_1.Router();
+//Login
+userRourtes.post('/login', (req, res) => {
+    const body = req.body;
+    usuario_model_1.Usuario.findOne({ email: body.email }, (err, userDB) => {
+        if (err)
+            throw err;
+        if (!userDB) {
+            return res.json({
+                ok: false,
+                mensaje: 'usuario / contrasena no son conrrectos'
+            });
+        }
+        if (userDB.comprarPassword(body.password)) {
+            const tokenUser = token_1.default.getJwtToken({
+                _id: userDB._id,
+                nombre: userDB.nombre,
+                email: userDB.email,
+                avatar: userDB.avatar
+            });
+            res.json({
+                ok: true,
+                token: tokenUser
+            });
+        }
+        else {
+            return res.json({
+                ok: false,
+                mensaje: 'usuario / contrasena no son conrrectos ***'
+            });
+        }
+    });
+    ////
+});
+//Crear un usuario
+userRourtes.post('/create', (req, res) => {
+    const user = {
+        nombre: req.body.nombre,
+        email: req.body.email,
+        password: bcrypt_1.default.hashSync(req.body.password, 10),
+        avatar: req.body.avatar
+    };
+    usuario_model_1.Usuario.create(user).then(userDB => {
+        const tokenUser = token_1.default.getJwtToken({
+            _id: userDB._id,
+            nombre: userDB.nombre,
+            email: userDB.email,
+            avatar: userDB.avatar
+        });
+        res.json({
+            ok: true,
+            token: tokenUser
+        });
+    }).catch(err => {
+        res.json({
+            ok: false,
+            err
+        });
+    });
+});
+//Actualizar Usuario
+userRourtes.post('/update', autenticacion_1.verificaToken, (req, res) => {
+    const user = {
+        nombre: req.body.nombre || req.usuario.nombre,
+        email: req.body.email || req.usuario.email,
+        avatar: req.body.avatar || req.Usuario.avatar
+    };
+    usuario_model_1.Usuario.findByIdAndUpdate(req.usuario._id, user, { new: true }, (err, userDB) => {
+        if (err)
+            throw err;
+        if (!userDB) {
+            return res.json({
+                ok: false,
+                mensaje: 'No exite un usuario con ese ID'
+            });
+        }
+        //Generar nuevo tokken
+        const tokenUser = token_1.default.getJwtToken({
+            _id: userDB._id,
+            nombre: userDB.nombre,
+            email: userDB.email,
+            avatar: userDB.avatar
+        });
+        res.json({
+            ok: true,
+            token: tokenUser
+        });
+    });
+    // res.json({
+    //   ok: true,
+    //   usuario: req.usuario
+    // });
+});
+userRourtes.get('/', [autenticacion_1.verificaToken], (req, res) => {
+    const usuario = req.usuario;
+    res.json({
+        ok: true,
+        usuario
+    });
+});
+exports.default = userRourtes;
